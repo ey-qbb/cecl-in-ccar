aedbd4f987b1d566a427388a1507b86c66d58b2f
ey-qbb <48965028+ey-qbb@users.noreply.github.com>
2019-03-26 17:42:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ output = {}
 #server = app.server
 
 server = flask.Flask(__name__)
-server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
+#server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, server=server, static_folder='assets')
 
 app.layout = html.Div([
